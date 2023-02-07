e8dcaa4137817843c44c11b1914c3514e7775a00
Mayar Mohsen <mayouraamein@gmail.com>
2023-02-07 12:42:42 +0200

    remove backend

--- vezeeta-backend/vezeeta.BL/Managers/CenterManager/ICenterManager.cs
+++
@@ -1,8 +0,0 @@
-using vezeeta.BL.DTOs.Center;
-
-namespace vezeeta.BL.Managers.CenterManger;
-
-public interface ICenterManager : IGenericManager<CenterDTO>
-{
-    public IEnumerable<CenterReadDto>? LoadData();
-}
--- vezeeta-backend/vezeeta.DBL/Repos/CenterRepo/ICenterRepo.cs
+++
@@ -1,6 +0,0 @@
-namespace vezeeta.DBL.Repos.CenterRepo;
-
-public interface ICenterRepo : IGenericRepo<Center> 
-{
-    public bool Find(Center center, bool checkName = true);
-}
